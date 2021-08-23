897c4082774dfaed445b7de95c5bd421d6ee3ca4
AhmadSaman <ahmad.00079821@gmail.com>
2021-08-23 16:46:28 +0300

    commit tree 83b5be92fe00a6be17e444afbe06ab637687b721

--- src/managePresents.js
+++ src/managePresents.js
@@ -1,3 +1,9 @@
 export function managePresents(state, action) {
-  // your code here
+	// your code here
+	switch (action.type) {
+		case "presents/increase":
+			return { numberOfPresents: state.numberOfPresents + 1 };
+		default:
+			return state;
+	}
 }
